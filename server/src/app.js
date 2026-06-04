1c65272ba265cfdfbb5f29a4165c1442a13aa8dc
vijendra8475 <vijjubca@gmail.com>
2026-06-05 01:21:26 +0530

    Bsic project setupe with mongoDB integration

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "WITS API Running",
+  });
+});
+
+export default app;
